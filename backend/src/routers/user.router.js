fe8d40dace0e84ed2b6feec5b24dae93bb0ea096
sudhaneupane <sudaneupane.11@gmail.com>
2024-11-17 14:51:09 +0545

    deleted unnecessary

--- backend/src/routers/user.router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createUser } from "../controllers/user.controller.js";
-
-const router = Router();
-
-router.route("/").post(createUser);
-
-export default router;
