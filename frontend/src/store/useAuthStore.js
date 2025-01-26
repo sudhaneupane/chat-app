ba5c972c7ac77dc619e9f3e057b89562120f0338
sudhaneupane <sudaneupane.11@gmail.com>
2025-01-26 21:56:54 +0545

    misc

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const response = await axiosInstance.get("/auth/check");
+      set({ authUser: response.data });
+    } catch (error) {
+      console.log("error in CHECKaUTH", error);
+
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
